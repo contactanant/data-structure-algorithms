cac34e1487f3cb1734dea0a59742232bc3584c8b
Anant Agarwal <contactanant@yahoo.com>
2015-09-27 12:39:22 +0100

    Anant | Added bubble sort generic algo

--- src/main/java/com/alph/excercise/BubbleSort.java
+++ src/main/java/com/alph/excercise/BubbleSort.java
@@ -1,9 +1,30 @@
 package com.alph.excercise;
 
+
 public class BubbleSort {
 
-    public int[] bubbleSort(int [] array) {
+    public <T> T[] bubbleSort(T[] array) {
 
+        if (array == null || array.length == 1) {
+            return array;
+        }
+
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (((Comparable) array[j]).compareTo(array[j + 1]) > 0) {
+                    swap(array, j, j + 1);
+                }
+            }
+        }
         return array;
+    }
+
+    private <T> void swap(T[] array, int i, int j) {
+        if (array.length -1 < (i + 1) || array.length <  (j + 1))
+            return;
+        T tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+        return;
     }
 }
