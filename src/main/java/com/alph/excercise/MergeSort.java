da3133312de679c4c1ad1656028451929b51d8f7
Anant Agarwal <contactanant@yahoo.com>
2015-10-04 21:19:49 +0100

    Anant | Added merge sort

--- src/main/java/com/alph/excercise/MergeSort.java
+++ src/main/java/com/alph/excercise/MergeSort.java
@@ -2,7 +2,38 @@ package com.alph.excercise;
 
 public class MergeSort {
 
-    public <T> T[] sort(T[] array) {
+    public int[] sort(int[] array) {
+        if (array.length < 2) {
+            return array;
+        }
+        int mid = array.length / 2;
+        int[] left = new int[mid];
+        System.arraycopy(array, 0, left, 0, mid);
+        sort(left);
+        int[] right = new int[array.length - mid];
+        System.arraycopy(array, mid, right, 0, array.length - mid);
+        sort(right);
+        merge(left, right, array);
         return array;
+    }
+
+    public void merge(int[] left, int[] right, int[] mergedArray) {
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int nLeft = left.length;
+        int nRight = right.length;
+
+        while (k < (nLeft + nRight)) {
+            if (j >= nRight || (i < nLeft && left[i] <= right[j])) {
+                mergedArray[k] = left[i];
+                i++;
+            } else {
+                mergedArray[k] = right[j];
+                j++;
+            }
+            k++;
+        }
     }
 }
--- src/test/groovy/com/alph/excercise/MergeSortTest.groovy
+++ src/test/groovy/com/alph/excercise/MergeSortTest.groovy
@@ -15,4 +15,17 @@ class MergeSortTest extends spock.lang.Specification {
         then:
         sortedArray == [1, 2, 3, 5, 10]
     }
+
+    def "should merge the two sorted array"() {
+        given:
+        int[] intArray1 = [5, 10]
+        int[] intArray2 = [1, 2, 3]
+        int[] finalArray = new int[intArray1.length + intArray2.length]
+
+        when:
+        mergeSort.merge(intArray1, intArray2, finalArray)
+
+        then:
+        finalArray == [1, 2, 3, 5, 10]
+    }
 }
